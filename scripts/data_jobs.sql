75692be0721ec46a9638e3230405fa8f512d4b1a
Patrick Shanks <patrick.shanks42@gmail.com>
2023-02-07 17:59:08 -0600

    finished 7

--- scripts/data_jobs.sql
+++ scripts/data_jobs.sql
@@ -5,6 +5,8 @@ FROM data_analyst_jobs;
 
 	--ANSWER: 1793
 
+
+
 --2.	Write a query to look at just the first 10 rows. What company is associated with the job posting on the 10th row?
 
 SELECT *
@@ -12,6 +14,8 @@ FROM data_analyst_jobs
 LIMIT 10;
 	
 	--ANSWER: ExxonMobil
+
+
 
 --3.	How many postings are in Tennessee? How many are there in either Tennessee or Kentucky?
 
@@ -24,7 +28,9 @@ FROM data_analyst_jobs
 WHERE location = 'KY' OR location ='TN';
 
 	--ANSWER: 21 in TN, 27 in TN or KY.
-	
+
+
+
 --4.	How many postings in Tennessee have a star rating above 4?
 
 SELECT COUNT (star_rating)
@@ -40,12 +46,48 @@ WHERE star_rating ISNULL;
 		SELECT COUNT (star_rating)
 		FROM data_analyst_jobs
 		WHERE star_rating ISNULL;
+			--this returns 0. 
+		
+		SELECT COUNT (*)
+		FROM data_analyst_jobs
+		WHERE star_rating ISNULL;
+			--I understand that these two queries are not equivalent, but I don't know why.
+
+
 
 --5.	How many postings in the dataset have a review count between 500 and 1000?
 
+SELECT COUNT (*)
+FROM data_analyst_jobs
+WHERE review_count BETWEEN 500 AND 1000;
+
+	--ANSWER: 151
+
+
+
 --6.	Show the average star rating for companies in each state. The output should show the state as `state` and the average rating for the state as `avg_rating`. Which state shows the highest average rating?
 
+SELECT location AS state, AVG(star_rating) AS avg_rating
+FROM data_analyst_jobs
+GROUP BY location
+ORDER BY avg_rating DESC;
+
+	--ANSWER: Nebraska with an average rating of 4.19. 
+	--FUN FACT: I had to look up which state is NE.
+
+
+
 --7.	Select unique job titles from the data_analyst_jobs table. How many are there?
+
+SELECT DISTINCT (title)
+FROM data_analyst_jobs;
+
+SELECT COUNT (DISTINCT (title))
+FROM data_analyst_jobs;
+
+	--ANSWER: 881 unique job titles.
+
+
 
 --8.	How many unique job titles are there for California companies?
 
